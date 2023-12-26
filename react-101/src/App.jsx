0060ba002197875daad60278dab0802a927c7454
Brajagopal Mukherjee <106516823+brajagopal21@users.noreply.github.com>
2023-12-26 06:52:31 +0000

    counter app

--- react-101/src/App.jsx
+++ react-101/src/App.jsx
@@ -1,7 +1,19 @@
+import { useState } from "react";
+import "./styles.css";
 function App() {
+  let [counter, setCounter] = useState(0);
+  // const addValue = () => {
+  //   setCounter(prev=> prev + 1);
+  // }
+  // const removeValue = () => {
+  //   setCounter(prev=> prev - 1);
+  // }
   return (
     <>
-    <p>Hello React</p>
+      <div>{counter} likes</div>
+      <span onClick={() => setCounter(prev=> prev + 1)}>👍🏽</span>
+      <span onClick={() => setCounter(prev=> prev - 1)}>👎🏽</span>
+      <h3>Like or dislike to increase/decrease</h3>
     </>
   )
 }
